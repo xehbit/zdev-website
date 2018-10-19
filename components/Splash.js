ab86233b478178bd747d77726edbec7b1f6c9f4c
Kevin van der Burgt <kevin@zdev.com>
2018-10-20 01:16:09 +0200

    Added a footer for the splashpage

--- components/Splash.js
+++ components/Splash.js
@@ -4,4 +4,15 @@ export default () => <section className="hero is-fullheight is-dark splash">
       <img src="/static/logo.png" />
     </div>
   </div>
+
+  <div className="hero-footer">
+    <div className="container has-text-centered" style={{
+      marginBottom: 10,
+      color: 'rgba(255, 255, 255, .5)',
+    }}>
+      <small>
+        <strong>KvK:</strong> 57772339 | <strong>BTW:</strong> NL211378136B01 | <strong>Email:</strong> info@zdev.com | <strong>Phone:</strong> +31 33 285 2009
+      </small>
+    </div>
+  </div>
 </section>;
